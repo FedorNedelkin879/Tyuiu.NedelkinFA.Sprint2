ff43de124aafb5decafe4dfed939d04ae735456e
Федя Неделькин <fffedosss898@gmail.com>
2024-10-22 22:33:31 +0500

    фывфы

--- Tyuiu.NedelkinFA.Sprint2.Task1.V8.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint2.Task1.V8.Lib/DataService.cs
@@ -8,11 +8,11 @@ namespace Tyuiu.NedelkinFA.Sprint2.Task1.V8.Lib
         {
             bool[] res = new bool[6];
             res[0] = (a > b) | (c < d);
-            res[1] = (a + 2 > b) & (c < d);
+            res[1] = (a > b + 1231231) & (c < d);
             res[2] = (a > b) || (c < d);
-            res[3] = (a + 2 > b) && (c < d);
-            res[4] = !(!res[0]);
-            res[5] = (a > b) ^ (c < d);
+            res[3] = (a > b + 123123123) && (c < d);
+            res[4] = !res[0];
+            res[5] = (a + 123123123> b) ^ (c < d);
             return res;
         }
     }
