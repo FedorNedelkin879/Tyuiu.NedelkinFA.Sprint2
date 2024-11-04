1b9e29b103ba0caa98d555808b256a4fe8f0e30a
Федя Неделькин <fffedosss898@gmail.com>
2024-11-04 22:07:26 +0500

    укепрпавы

--- Tyuiu.NedelkinFA.Sprint2.Task3.V3.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint2.Task3.V3.Lib/DataService.cs
@@ -9,35 +9,29 @@ namespace Tyuiu.NedelkinFA.Sprint2.Task3.V3.Lib
 
             if (x > 1)
             {
-                // Попробуем вариант с изменением порядка действий для вычисления (x + 1) / (x - 1)
-                double fraction = (x + 1.0) / (x - 1.0);
-                y = Math.Pow(fraction, x);
+                y = Math.Pow((x + 1) / (x - 1), x);
             }
             else
             {
                 if (x == 0)
                 {
-                    // Формула для x = 0
                     y = (Math.Pow(x, 4) - Math.Cos(Math.Pow(x, 5)) + 3) / (Math.Pow(x, 2) - Math.Sin(Math.Pow(x, 2)) + 12);
                 }
                 else
                 {
                     if (x > -7 && x < 0)
                     {
-                        // Формула для -7 < x < 0
                         y = Math.Pow(1 + Math.Sqrt(x + 3) / Math.Pow(x, 2), x);
                     }
                     else
                     {
                         if (x <= -7)
                         {
-                            // Формула для x <= -7
-                            y = Math.Pow(x, 2) + 10 * x - (1.0 / x);
+                            y = Math.Pow(x, 2) + 10 * x - (1 / x);
                         }
                     }
                 }
             }
-
             return Math.Round(y, 3);
         }
     }
