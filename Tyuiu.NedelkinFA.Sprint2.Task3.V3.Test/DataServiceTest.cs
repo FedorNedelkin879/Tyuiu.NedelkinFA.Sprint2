687e7f751a9c7f686b5ec34525ed27b33bbddc0c
Федя Неделькин <fffedosss898@gmail.com>
2024-11-04 21:53:42 +0500

    dfgfdg

---
+++ Tyuiu.NedelkinFA.Sprint2.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+using Tyuiu.NedelkinFA.Sprint2.Task3.V3.Lib;
+
+namespace Tyuiu.NedelkinFA.Sprint2.Task3.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.333;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double res = ds.Calculate(x);
+            double wait = -0.909;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -6;
+            double res = ds.Calculate(x);
+            double wait = -2.333;
+            Assert.AreEqual(wait, res);
+        }
+}
---
+++ Tyuiu.NedelkinFA.Sprint2.Task3.V3/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.NedelkinFA.Sprint2.Task3.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнил: Неделькин Ф. А. | ИИПБ-24-1";
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("* Спринт #2                                                                                    *");
+Console.WriteLine("* Тема:                                                                                        *");
+Console.WriteLine("* Задание #                                                                                    *");
+Console.WriteLine("* Вариант #                                                                                    *");
+Console.WriteLine("* Выполнил: Неделькин Ф. А. | ИИПБ-24-1                                                        *");
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                                     *");
+Console.WriteLine("*                                                                                              *");
+Console.WriteLine("*                                                                                              *");
+Console.WriteLine("*                                                                                              *");
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                             *");
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("*                                                                                              *");
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                   *");
+Console.WriteLine("************************************************************************************************");
+Console.WriteLine("napishi x");
+double x = Convert.ToDouble(Console.ReadLine());
+double res = ds.Calculate(x);
+
+Console.WriteLine("zn funk " + res);
+
+Console.ReadKey();
