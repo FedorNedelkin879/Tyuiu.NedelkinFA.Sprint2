66010521608a04ab313cca2853ab84a8a8d104cd
Федя Неделькин <fffedosss898@gmail.com>
2024-11-26 20:23:39 +0500

    dhgf

--- Tyuiu.NedelkinFA.Sprint2.Task6.V12.Lib/DataService.cs
+++ Tyuiu.NedelkinFA.Sprint2.Task6.V12.Lib/DataService.cs
@@ -6,8 +6,10 @@ namespace Tyuiu.NedelkinFA.Sprint2.Task6.V12.Lib
     {
         public string FindDateOfPreviousDay(int g, int m, int n)
         {
-            if (m < 1 || m > 12 || n < 1 || n > 31)
-                throw new ArgumentException("Некорректные значения даты.");
+            if (m < 1 || m > 12)
+                throw new ArgumentException($"Неверный месяц: {m}");
+            if (n < 1 || n > 31)
+                throw new ArgumentException($"Неверный день: {n}");
 
             if (n == 1)
             {
@@ -23,7 +25,7 @@ namespace Tyuiu.NedelkinFA.Sprint2.Task6.V12.Lib
                     1 or 3 or 5 or 7 or 8 or 10 or 12 => 31,
                     4 or 6 or 9 or 11 => 30,
                     2 => 29,
-                    _ => throw new ArgumentException("Некорректный месяц.")
+                    _ => throw new ArgumentException($"Некорректный месяц: {m}")
                 };
             }
             else
@@ -31,24 +33,7 @@ namespace Tyuiu.NedelkinFA.Sprint2.Task6.V12.Lib
                 n--;
             }
 
-            string monthName = m switch
-            {
-                1 => "янв",
-                2 => "фев",
-                3 => "мар",
-                4 => "апр",
-                5 => "май",
-                6 => "июн",
-                7 => "июл",
-                8 => "авг",
-                9 => "сен",
-                10 => "окт",
-                11 => "ноя",
-                12 => "дек",
-                _ => throw new ArgumentException("Некорректный месяц.")
-            };
-
-            return $"{n} {monthName} {g}";
+            return $"{n:00}.{m:00}.{g}";
         }
     }
 }
